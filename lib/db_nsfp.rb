f322790fb5fd29843c1f5147ab09990014293e4e
Miguel Vazquez <miguel.vazquez@cnio.es>
2014-06-24 00:16:33 +0200

    nil

--- lib/db_nsfp.rb
+++ lib/db_nsfp.rb
@@ -5,8 +5,29 @@ module DbNSFP
   extend Resource
   self.subdir = 'var/dbNSFP'
 
+  MI_SHARD_FUNCTION = Proc.new do |key|
+    key[(13..14)]
+  end
+
+  GM_SHARD_FUNCTION = Proc.new do |key|
+    key[0..key.index(":")-1]
+  end
+
+  MI_SCORES = %w(SIFT_score SIFT_converted_rankscore SIFT_pred Polyphen2_HDIV_score
+                 Polyphen2_HDIV_rankscore Polyphen2_HDIV_pred Polyphen2_HVAR_score
+                 Polyphen2_HVAR_rankscore Polyphen2_HVAR_pred LRT_score LRT_converted_rankscore
+                 LRT_pred MutationTaster_score MutationTaster_converted_rankscore
+                 MutationTaster_pred MutationAssessor_score MutationAssessor_rankscore
+                 MutationAssessor_pred FATHMM_score FATHMM_rankscore FATHMM_pred RadialSVM_score
+                 RadialSVM_rankscore RadialSVM_pred LR_score LR_rankscore LR_pred
+                 Reliability_index VEST3_score VEST3_rankscore)
+
+  def self.organism
+    "Hsa/dec2013"
+  end
+
   DbNSFP.claim DbNSFP.data, :proc do |directory|
-    url = "http://dbnsfp.houstonbioinformatics.org/dbNSFPzip/dbNSFPv2.1.zip"
+    url = "http://dbnsfp.houstonbioinformatics.org/dbNSFPzip/dbNSFPv2.5.zip"
     Misc.in_dir(directory) do
       FileUtils.mkdir_p '.source'
       `wget '#{url}' -c -O .source/pkg.zip && cd .source && unzip pkg.zip && find . -name '*variant*' | xargs -I '{}' mv '{}' ../ && cd .. && rm -Rf .source *.zip`
@@ -14,28 +35,44 @@ module DbNSFP
     nil
   end
 
-  def self.database
-    @@database ||= begin
-                     sharder_function = Proc.new do |key|
-                       key[(13..14)]
-                     end
+  DbNSFP.claim DbNSFP.mutations, :proc do |filename|
+    Misc.sensiblewrite(filename) do |f|
+      DbNSFP.data.glob('*variant*').each do |file|
+        next unless file =~ /chr19/
+        TSV.traverse file, :type => :array, :bar => file do |line|
+          line = line.strip.gsub(/\t\.\t/, "\t\t")
+          if line =~ /^#/
+            f.puts("#: :type=:list#:namespace=#{organism}")
+            f.puts("#Genomic Mutation" << "\t" << line[1..-1])
+          else
+            chr, pos, ref, alt = line.split("\t")
+            f.puts(([chr, pos, alt] * ":" ) << "\t" << line)
+          end
+        end
+      end
+    end
+  end
 
-                     Persist.persist_tsv("dbNSFP", nil, {}, 
-                                         :file => DbNSFP.scores_shard.find, :persist => true, :prefix => "dbNSFP", :serializer => :float_array,
-                                         :shard_function => sharder_function) do |sharder|
+  DbNSFP.claim DbNSFP.data.readme, :url, "http://dbnsfp.houstonbioinformatics.org/dbNSFPzip/dbNSFP2.5.readme.txt"
+
+  def self.database
+    @@database||= begin
+                     Persist.persist_tsv("dbNSFP", nil, {}, :persist => true, :update => false,
+                                         :file => DbNSFP.scores_shard.find, :prefix => "dbNSFP", :serializer => :float_array, :engine => "BDB",
+                                         :shard_function => MI_SHARD_FUNCTION) do |sharder|
 
                        require 'rbbt/sources/organism'
 
                        organism = "Hsa/jan2013"
 
-                       files = DbNSFP.data.produce.glob('*variant*')
+                       files = DbNSFP.data.produce.glob('*variant*')#.select{|f| f =~ /22/}
 
                        transcript2protein = Organism.transcripts(organism).tsv :fields => ["Ensembl Protein ID"], :type => :single, :persist => true, :unnamed => true
 
                        save_header = true
                        TSV.traverse files.sort, :bar => "DbNSFP files" do |file|
                          all_fields = TSV.parse_header(file).all_fields
-                         scores = all_fields.select{|f| f =~ /score/}
+                         scores = all_fields[23..-1]
 
                          if save_header
                            sharder.fields = scores
@@ -47,13 +84,13 @@ module DbNSFP
                          transcript_field = all_fields.index "Ensembl_transcriptid"
                          score_fields = scores.collect{|f| all_fields.index f}
 
-                         sharder.write
-                         TSV.traverse file, :type => :array, :bar => File.basename(file), :into => sharder do |line|
+                         TSV.traverse file, :type => :array, :bar => File.basename(file) do |line|
                            next if line[0] == "#"
+
                            parts = line.strip.split("\t",-1)
                            transcripts = parts[transcript_field].split ";"
 
-                           if transcripts.length == 1
+                           res = if transcripts.length == 1
                              transcript = transcripts.first
                              protein = transcript2protein[transcript]
                              next if protein.nil? or protein.empty?
@@ -66,7 +103,8 @@ module DbNSFP
                              isoform = protein + ":" << mutation_parts * ""
                              values = scores.collect{|s| s == '.' ? -999 : s.to_f }
 
-                             [isoform, values]
+                             sharder[isoform] = values
+                             #[isoform, values]
                            else
                              proteins = transcript2protein.values_at *transcripts
                              next if proteins.compact.empty?
@@ -75,7 +113,7 @@ module DbNSFP
                              next if mutation_parts[1] == "-1"
 
                              if mutation_parts[1].index ";"
-                               mutations_zip = mutation_parts[1].split(";").collect{|pos| [m[0],pos,m[2]] }
+                               mutations_zip = mutation_parts[1].split(";").collect{|pos| [mutation_parts[0],pos,mutation_parts[2]] }
                              else
                                mutations_zip = [mutation_parts] * proteins.length
                              end
@@ -84,23 +122,23 @@ module DbNSFP
 
                              scores_zip = [s] * proteins.length
 
-                             results = [].extend TSV::MultipleResult
+                             #results = [].extend MultipleResult
+
                              transcripts.each_with_index do |transcript,i|
                                protein = proteins[i]
                                next if protein.nil? or protein.empty?
                                isoform = protein + ":" << (mutations_zip[i] * "")
                                values = scores_zip[i].collect{|s| s == '.' ? -999 : s.to_f }
-                               results << [isoform, values]
+                               sharder[isoform] = values
+                               #results << [isoform, values]
                              end
 
-                             results
+                             #results
                            end
                          end
                        end # traverse files
 
-                       sharder.read; 
                      end # persist
                    end # end
   end
-
 end
--- workflow.rb
+++ workflow.rb
@@ -12,7 +12,7 @@ Workflow.require_workflow "Genomics"
 require 'rbbt/entity/protein'
 require 'rbbt/util/R'
 
-require 'db_nsfp'
+#require 'db_nsfp'
 
 module MutEval
   extend Workflow
@@ -29,35 +29,6 @@ module MutEval
   }
 
   CACHES.values.each{|db| db.close}
-
-  helper :get_dbNSFP do |mutations,method|
-    mutations = mutations.select{|m| m =~ /:([A-Z])\d+([A-Z])/ and $1 != $2 }.uniq.sort
-    field = case method.to_s
-            when "all"
-              nil
-            when "mutation_assessor"
-              "MutationAssessor_score_converted"
-            when "sift"
-              "SIFT_score_converted"
-            when "polyphen"
-              "Polyphen2_HDIV_score"
-            when "ltr"
-              "LRT_score_converted"
-            when "mutation_tasser"
-              "MutationTaster_score_converted"
-            when "fathmm"
-              "FATHMM_score_converted"
-            end
-    Log.low "Querying dbNSFP (#{method || "all"}) with #{mutations.length} mutations"
-    database = DbNSFP.database
-    database.read_and_close do
-      if field.nil?
-        database.select(:key => mutations)
-      else
-        database.select(:key => mutations).slice(field)
-      end
-    end
-  end
 
   helper :get_cache do |method, mutations|
     tsv = TSV.setup({})
@@ -121,346 +92,47 @@ module MutEval
     end
   end
 
-  input :mutations, :array, "Protein Mutations in any supported format, preferably 'UniProt/SwissProt ID'; example ATS2_HUMAN:G872R"
-  input :organism, :string, "Organism code", "Hsa"
-  task :transFIC => :tsv do |mutations, organism|
-    cached, mutations = get_cache(:transFIC, mutations)
-
-    missing = get_missing(:transFIC, mutations)
-    mutations = mutations - missing
-
-    proteins = mutations.collect{|mutation| mutation.split(":").first}
-    index = Organism.protein_identifiers(organism).index(:target => "Ensembl Protein ID", :persist => true)
-    proteins2native_id = Misc.process_to_hash(proteins){|list| index.chunked_values_at(list)}
-
-    all_mutations = []
-    mutations.each do |mutation|
-      protein, change = mutation.split(":")
-      native_id = proteins2native_id[protein]
-      all_mutations << [native_id, change] * ":"
+  helper :get_dbNSFP do |mutations,method|
+    mutations = mutations.select{|m| m =~ /:([A-Z])\d+([A-Z])/ and $1 != $2 }.uniq.sort
+    field = case method.to_s
+            when "all"
+              nil
+            when "mutation_assessor"
+              "MutationAssessor_score_converted"
+            when "sift"
+              "SIFT_score_converted"
+            when "polyphen"
+              "Polyphen2_HDIV_score"
+            when "ltr"
+              "LRT_score_converted"
+            when "mutation_tasser"
+              "MutationTaster_score_converted"
+            when "fathmm"
+              "FATHMM_score_converted"
+            end
+    Log.low "Querying dbNSFP (#{method || "all"}) with #{mutations.length} mutations"
+    database = DbNSFP.database
+    database.read_and_close do
+      if field.nil?
+        database.select(:key => mutations)
+      else
+        database.select(:key => mutations).slice(field)
+      end
     end
-
-    fields = %w( siftTransfic siftTransficLabel pph2Transfic pph2TransficLabel maTransfic maTransficLabel)
-    tsv = TSV.setup({}, :key_field => "Protein Mutation", :fields => fields, :type => :list)
-
-    if all_mutations.any?
-      server_predictions = TransFIC.chunked_predict(all_mutations, 1000)
-
-      mutations.each do |mutation|
-        protein, change = mutation.split(":")
-        native_id = proteins2native_id[protein]
-        code = [native_id, change] * ":"
-        values = server_predictions[code]
-        next if values.nil?
-        tsv[mutation] = values
-      end unless server_predictions.nil?
-
-      add_cache(:transFIC, tsv)
-      cached.each do |mutation,values| tsv[mutation] = values end
-    else
-      cached.each do |mutation,values| tsv[mutation] = values end
-    end
-
-    add_missing(:transFIC, mutations, tsv)
-
-    tsv
   end
-  export_synchronous :transFIC
-
-
-  input :mutations, :array, "Protein Mutations in any supported format, preferably 'UniProt/SwissProt ID'; example ATS2_HUMAN:G872R"
-  input :organism, :string, "Organism code", "Hsa"
-  task :mutation_assessor => :tsv do |mutations, organism|
-    cached, mutations = get_cache(:mutation_assessor, mutations)
-
-    missing = get_missing(:mutation_assessor, mutations)
-    mutations = mutations - missing
-
-    proteins = mutations.collect{|mutation| mutation.split(":").first}
-    index = Organism.protein_identifiers(organism).index(:target => "UniProt/SwissProt ID", :persist => true)
-    proteins2native_id = Misc.process_to_hash(proteins){|list| index.chunked_values_at(list)}
-
-    all_mutations = {}
-    mutations.each do |mutation|
-      protein, change = mutation.split(":")
-      native_id = proteins2native_id[protein]
-      all_mutations[native_id] ||= []
-      all_mutations[native_id] << change
-    end
-
-    server_predictions = MutationAssessor.chunked_predict(all_mutations, 1000)
-
-    tsv = TSV.setup({}, :key_field => "Protein Mutation", :fields => ["Mutation Assessor Prediction", "Mutation Assessor Score"], :type => :list)
-    mutations.each do |mutation|
-      protein, change = mutation.split(":")
-      native_id = proteins2native_id[protein]
-      code = [native_id, change] * " "
-      values = server_predictions[code]
-      next if values.nil?
-      tsv[mutation] = values.values_at "Func. Impact", "FI score"
-    end
-
-    add_cache(:mutation_assessor, tsv)
-    cached.each do |mutation,values| tsv[mutation] = values end
-
-    add_missing(:mutation_assessor, mutations, tsv)
-
-    tsv
-  end
-  export_synchronous :mutation_assessor
-
-  input :mutations, :array, "Protein Mutations in any supported format, preferably 'Ensembl Protein ID'; example ENSP00000251582:G872R"
-  input :organism, :string, "Organism code", "Hsa"
-  task :sift => :tsv do |mutations, organism|
-    cached, mutations = get_cache(:sift, mutations)
-
-    missing = get_missing(:sift, mutations)
-    mutations = mutations - missing
-
-    proteins = mutations.collect{|mutation| mutation.split(":").first}
-    index = Organism.protein_identifiers(organism).index(:target => "Ensembl Protein ID", :persist => true)
-    proteins2native_id = Misc.process_to_hash(proteins){|list| index.chunked_values_at(list)}
-
-    all_mutations = []
-    mutations.each do |mutation|
-      protein, change = mutation.split(":")
-      native_id = proteins2native_id[protein]
-      all_mutations << [native_id, change] * ":"
-    end
-
-    log(:prediction, "Calling predictor")
-    server_predictions = SIFT.chunked_predict(all_mutations)
-
-    tsv = TSV.setup({}, :key_field => "Protein Mutation", :fields => ["SIFT Prediction", "SIFT Score"], :type => :list)
-    mutations.each do |mutation|
-      protein, change = mutation.split(":")
-      native_id = proteins2native_id[protein]
-      code = [native_id, change] * ":"
-      values = server_predictions[code]
-      next if values.nil?
-      tsv[mutation] = values.values_at "Prediction", "Score 1"
-    end
-
-    log(:cache, "Adding to cache")
-    add_cache(:sift, tsv)
-
-    cached.each do |mutation,values| tsv[mutation] = values end
-
-    add_missing(:sift, mutations, tsv)
-
-    tsv
-  end
-  export_synchronous :sift
-
-  input :mutations, :array, "Protein Mutations in any supported format, preferably 'UniProt/SwissProt Accession'; example O95450:G872R"
-  input :organism, :string, "Organism code", "Hsa"
-  task :polyphen => :tsv do |mutations, organism|
-    cached, mutations = get_cache(:polyphen, mutations)
-
-    missing = get_missing(:polyphen, mutations)
-    mutations = mutations - missing
-
-    proteins = mutations.collect{|mutation| mutation.split(":").first}
-    index = Organism.protein_identifiers(organism).index(:target => "UniProt/SwissProt Accession", :persist => true)
-    proteins2native_id = Misc.process_to_hash(proteins){|list| index.chunked_values_at(list)}
-
-    all_mutations = []
-    mutations.each do |mutation|
-      protein, change = mutation.split(":")
-      native_id = proteins2native_id[protein]
-      next unless change.match(/([A-Z*])(\d+)([A-Z*])/)
-      wt, pos, mt = change.match(/([A-Z*])(\d+)([A-Z*])/).values_at 1,2,3
-      all_mutations << [native_id, pos, wt, mt] * " "
-    end
-
-    server_predictions = Polyphen2::Batch.chunked_predict(all_mutations * "\n") 
-
-    tsv = TSV.setup({}, :key_field => "Protein Mutation", :fields => ["Polyphen Prediction", "Polyphen Score"], :type => :list)
-    mutations.each do |mutation|
-      protein, change = mutation.split(":")
-      native_id = proteins2native_id[protein]
-      code = [native_id, change] * ":"
-      values = server_predictions[code]
-      next if values.nil?
-      tsv[mutation] = values.values_at "prediction", "pph2_prob"
-    end
-
-    add_cache(:polyphen, tsv)
-    cached.each do |mutation,values| tsv[mutation] = values end
-
-    add_missing(:polyphen, mutations, tsv)
-
-    tsv
-  end
-  export_synchronous :polyphen
-
-
-  input :mutations, :array, "Protein Mutations in any supported format, preferably 'UniProt/SwissProt Accession'; example O95450:G872R"
-  input :organism, :string, "Organism code", "Hsa"
-  task :snps_and_go => :tsv do |mutations, organism|
-    cached, mutations = get_cache(:snps_and_go, mutations)
-
-    missing = get_missing(:snps_and_go, mutations)
-    mutations = mutations - missing
-
-    proteins = mutations.collect{|mutation| mutation.split(":").first}
-    index = Organism.protein_identifiers(organism).index(:target => "UniProt/SwissProt Accession", :persist => true)
-    proteins2native_id = Misc.process_to_hash(proteins){|list| index.chunked_values_at(list)}
-
-    all_mutations = []
-
-    mutations.each do |mutation|
-      acc, change = mutation.split(":")
-      all_mutations << [acc, change] * ":"
-    end
- 
-    server_predictions = Misc.process_to_hash(all_mutations){|list|
-      list.collect{|mut|
-        protein, change = mut.split(":")
-        acc = proteins2native_id[protein]
-        begin
-          raise "No translation for #{ protein }" if acc.nil?
-          SNPSandGO.predict(acc, change)
-        rescue
-          Log.debug("Error in SNPs&GO: #{$!.message}")
-          nil
-        end
-      }
-    }
-
-    tsv = TSV.setup({}, :key_field => "Protein Mutation", :fields => ["SNPSandGO Prediction", "SNPSandGO Score"], :type => :list)
-    mutations.each do |mutation|
-      values = server_predictions[mutation]
-      next if values.nil?
-      tsv[mutation] = values
-    end
-
-    add_cache(:snps_and_go, tsv)
-    cached.each do |mutation,values| tsv[mutation] = values end
- 
-    all_mutations = []
-
-    add_missing(:snps_and_go, mutations, tsv)
-
-    tsv
-  end
-  export_synchronous :snps_and_go
-
-
-  #{{{ FEATURES
-  
-  task :training_features => :tsv do
-    dataset = clean_name
-    filename = File.join(File.dirname(__FILE__), 'data', dataset + '.txt')
-    organism = "Hsa/may2012"
-
-    dataset_tsv = TSV.open(filename, :type => :list, :key_field => 1)
-    dataset_tsv.fields = ["Pathogenic"]
-    dataset_tsv.key_field = "Protein Mutation"
-
-    all_mutations = dataset_tsv.keys
-
-    dataset_tsv = dataset_tsv.attach MutEval.job(:mutation_assessor, clean_name, :mutations => all_mutations, :organism => organism).run
-    dataset_tsv = dataset_tsv.attach MutEval.job(:sift, clean_name, :mutations => all_mutations, :organism => organism).run
-    dataset_tsv = dataset_tsv.attach MutEval.job(:snps_and_go, clean_name, :mutations => all_mutations, :organism => organism).run
-    dataset_tsv = dataset_tsv.attach MutEval.job(:polyphen, clean_name, :mutations => all_mutations, :organism => organism).run
-
-
-    dataset_tsv.to_s
-  end
-
-  input :mutations, :array, "Mutations to evaluate", nil 
-  task :features => :tsv do |mutations|
-    organism = "Hsa/may2012"
-
-    mutations = Misc.process_to_hash(mutations){|list| list.collect{ [] }}
-    dataset_tsv = TSV.setup(mutations, :type => :list)
-    dataset_tsv.key_field = "Protein Mutation"
-    dataset_tsv.fields = []
-
-    all_mutations = dataset_tsv.keys
-
-    dataset_tsv = dataset_tsv.attach MutEval.job(:mutation_assessor, clean_name, :mutations => all_mutations, :organism => organism).run
-    dataset_tsv = dataset_tsv.attach MutEval.job(:sift, clean_name, :mutations => all_mutations, :organism => organism).run
-    dataset_tsv = dataset_tsv.attach MutEval.job(:snps_and_go, clean_name, :mutations => all_mutations, :organism => organism).run
-    dataset_tsv = dataset_tsv.attach MutEval.job(:polyphen, clean_name, :mutations => all_mutations, :organism => organism).run
-
-    dataset_tsv.to_s
-  end
-  export_synchronous :features
-
-  dep do |jobname, inputs| MutEval.job(:training_features, jobname) end
-  task :rpart_model => :binary do
-    feature_file = step(:training_features).path
-
-    R.run <<-EOF
-
-library('rpart');
-features = rbbt.tsv('#{feature_file}');
-names(features) = make.names(names(features))
-
-model = rpart(Pathogenic ~ Mutation.Assessor.Score + Polyphen.Score + SIFT.Score + SNPSandGO.Score, data = features, method='class',control=rpart.control(cp=.0001))
-
-save(file='#{path}', model)
-    EOF
-    nil
-  end
-
-
-  dep do |jobname, inputs| MutEval.job(:training_features, jobname) end
-  task :svm_model => :binary do
-    feature_file = step(:training_features).path
-
-    R.run <<-EOF
-
-library('e1071');
-features = rbbt.tsv('#{feature_file}');
-names(features) = make.names(names(features))
-
-model = svm(Pathogenic ~ Mutation.Assessor.Score + Polyphen.Score + SIFT.Score, data = features, na.action=na.pass)
-
-save(file='#{path}', model)
-    EOF
-    nil
-  end
-
-  dep :features
-  input :dataset, :string
-  dep do |jobname, inputs| MutEval.job(:rpart_model, inputs[:dataset]) end
-  task :predict => :tsv do |mutation|
-    model_file = step(:rpart_model).path
-    feature_file = step(:features).path
-
-    R.run <<-EOF
-
-library('e1071');
-library('randomForest');
-library('rpart');
-
-load(file='#{model_file}');
-features = rbbt.tsv('#{feature_file}');
-names(features) = make.names(names(features));
-
-features[, "Mutation.Assessor.Score"] = as.numeric(features[,"Mutation.Assessor.Score"])
-features[, "Polyphen.Score"]          = as.numeric(features[,"Polyphen.Score"])
-features[, "SIFT.Score"]              = as.numeric(features[,"SIFT.Score"])
-features[, "SNPSandGO.Score"]         = as.numeric(features[,"SNPSandGO.Score"])
-
-d = as.data.frame(predict(model, features));
-names(d) <- c("Neutral", "Pathogenic");
-
-rbbt.tsv.write(file='#{self.path}', d, key.field = "Protein Mutation");
-    EOF
-
-    nil
-  end
-  export_synchronous :predict
 
   input :mutations, :array, "Mutated Isoforms", nil
-  input :method, :select, "Method to repot", "all", :select_options => %w(all mutation_assessor sift polyphen)
-  task :dbNSFP => :tsv do |mutations,method|
-    res =  get_dbNSFP(mutations, method)
-    res
+  task :dbNSFP => :tsv do |mutations|
+    database = DbNSFP.database
+    database.unnamed = true
+    dumper = TSV::Dumper.new :key_field => "Mutated Isoform", :fields => database.fields, :type => :list, :cast => :to_f
+    dumper.init
+    TSV.traverse mutations, :into => dumper, :bar => true, :type => :array do |mutation|
+      p = database[mutation]
+      next if p.nil?
+      p.collect!{|v| v == -999 ? nil : v }
+      [mutation, p]
+    end
   end
-  export_asynchronous :dbNSFP
+  export_synchronous :dbNSFP
 end
